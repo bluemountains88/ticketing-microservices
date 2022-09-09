0c1d351531ffe36914511a3f3c64f8f9c5732c53
bluemountains88 <m.dariouk@gmail.com>
2022-09-09 15:20:44 -0300

    Add session support by cookies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import 'express-async-errors';
 
@@ -9,7 +10,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true); //by nginx
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true, 
+    })
+);
 
 app.use(indexRouter);
 app.all('*', async () => { throw new NotFoundError() });
