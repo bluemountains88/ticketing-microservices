5e9a42690cbaebe192de2ced353185fd4e6fc4bf
bluemountains88 <m.dariouk@gmail.com>
2022-09-10 13:15:00 -0300

    Refactor the way to get a cookie

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => { 
-    const authResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'admin'
-        })
-        expect(201);
-    const cookie = authResponse.get('Set-Cookie');
+    const cookie = await global.signin();
 
     const response = await request(app)
         .get('/api/users/currentuser')
@@ -18,6 +11,14 @@ it('responds with details about the current user', async () => {
         .expect(200);
     
     expect(response.body.currentUser.email).toEqual('test@test.com');    
+});
+
+it('responds with null if not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200);
+    expect(response.body.currentUser).toEqual(null);
 })
 
 
