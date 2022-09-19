07d697eeab5798e51f87158b67a3c704c1ab539d
bluemountains88 <m.dariouk@gmail.com>
2022-09-19 11:57:16 -0300

    Implements order events publishers

---
+++ orders/src/events/publishers/order-created-publisher.ts
@@ -0,0 +1,10 @@
+import { 
+    Publisher,
+    OrderCreatedEvent,
+    Subjects
+} from '@bluemountains/common';
+
+export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
+    readonly subject = Subjects.OrderCreated;
+};
+
