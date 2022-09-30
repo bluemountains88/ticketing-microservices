0f414b08a82f434d7ee892582edf5f8fc39bdc1d
bluemountains88 <m.dariouk@gmail.com>
2022-09-30 09:09:10 -0300

    try catch added

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
 const buildClient = ({ req }) => {
-    if (typeof window === 'undefined') {
-        return axios.create({
-            baseURL: 'http://www.argentickets.com.ar',
-            headers: req.headers
-        });
-    } else {
-        return axios.create({
-            baseUrl: '/'
-        });
-    }
-}
+    try {
+        if (typeof window === 'undefined') {
+            return axios.create({
+                baseURL: 'http://www.argentickets.com.ar',
+                headers: req.headers
+            });
+        } else {
+            return axios.create({
+                baseUrl: '/'
+            });
+        }
+    } catch (err) {
+        console.error(err);
+    };
+};
 
 export default buildClient;
