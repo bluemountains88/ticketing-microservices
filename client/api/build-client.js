6d61431edd9e1ca49d2bdaf9197faf443272acc4
bluemountains88 <m.dariouk@gmail.com>
2022-10-06 10:56:06 -0300

    fix

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
 const buildClient = ({ req }) => {
-  if (typeof window === "undefined") {
-    // We are on the server
-    try {
-      return axios.create({
-        baseURL: "http://www.argentickets.com.ar",
-        headers: req.headers,
-      });
-      } else {
+  try{
+    if (typeof window === "undefined") {
+      // We are on the server
+       return axios.create({
+          baseURL: "http://www.argentickets.com.ar",
+          headers: req.headers,
+        });
+    } else {
       // We must be on the browser
       return axios.create({
         baseUrl: "/",
       });
-    } catch (err) {
-      console.log(err);
     }
-  }
+  } catch (err) { console.log(err); };
 };
 
 export default buildClient;
