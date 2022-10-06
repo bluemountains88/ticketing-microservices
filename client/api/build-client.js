e5bfdd87d89c2bffdeefa6c0814213461e1a9c20
Dario <99050545+bluemountains88@users.noreply.github.com>
2022-10-06 09:38:54 -0300

    Update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,16 +3,19 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
-    return axios.create({
-      baseURL: "http://www.argentickets.com.ar",
-      headers: req.headers,
-    });
-  } else {
-    // We must be on the browser
-    return axios.create({
-      baseUrl: "/",
-    });
+    try {
+      return axios.create({
+        baseURL: "http://www.argentickets.com.ar",
+        headers: req.headers,
+      });
+      } else {
+      // We must be on the browser
+      return axios.create({
+        baseUrl: "/",
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
 
